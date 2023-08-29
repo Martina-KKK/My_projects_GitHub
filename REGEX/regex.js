0e6f77c4bef9af92b67b6c3dd95a0a441b2220b0
Martina-KKK <m.karrmannova@gmail.com>
2023-08-30 01:37:50 +0200

    Šifrování AES a Regulární výrazy

--- REGEX/regex.js
+++ REGEX/regex.js
@@ -1,4 +1,6 @@
+// funkce pro registraci
 function register() {
+  // identifikace vstupu podle id z html stranky
   var emailInput = document.getElementById('emailInput');
   var passwordInput = document.getElementById('passwordInput');
   var phoneInput = document.getElementById('phoneInput');
@@ -9,22 +11,26 @@ function register() {
   var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
   var phoneRegex = /^\+\d{1,3}\s?\d{3}\s?\d{3}\s?\d{3}$/;
 
+  // zadavani hodnot do vstupu
   var email = emailInput.value;
   var password = passwordInput.value;
   var phone = phoneInput.value;
 
+  // kontrola validace emailu
   if (!emailRegex.test(email)) {
     result.innerHTML = "E-mailová adresa je neplatná.";
     result.style.color = "red";
     return;
   }
 
+  // kontrola validace hesla
   if (!passwordRegex.test(password)) {
     result.innerHTML = "Heslo musí obsahovat alespoň 8 znaků, včetně číslic a malých a velkých písmen.";
     result.style.color = "red";
     return;
   }
 
+  // kontrola validace telefonniho cisla
   if (!phoneRegex.test(phone)) {
     result.innerHTML = "Telefonní číslo je neplatné. Zadejte telefonní číslo i s předvolbou.";
     result.style.color = "red";
@@ -34,4 +40,7 @@ function register() {
   // registrace uspesna
   result.innerHTML = "Registrace proběhla úspěšně!";
   result.style.color = "green";
+
+  // otevre okno s jinou html strankou
+  var newWindow = window.open("SIFROVANI_AES/Sifrovani.html", "_blank");
 }
